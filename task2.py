0727912a15307d693463f2e66bea7844b4232fb5
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2017-12-18 11:39:30 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -2,14 +2,18 @@ import pprint
 import json
 from urllib.request import urlopen
 
-id = input()
-request = "https://api.vk.com/method/users.get?user_ids={id}&v=5.8&fields=status".format(id = id)
+def user_vk(id):
+    request = "https://api.vk.com/method/users.get?user_ids={id}&v=5.8&fields=status".format(id = id)
 
-try:
-    request_obj = urlopen(request)
-	obj = json.loads(request_obj.read())
-except:
-	print('Ошибка!')
+    try:
+        request_obj = urlopen(request)
+        obj = json.loads(request_obj.read())
+    except:
+        print('Ошибка!')
+    return obj
 
-print('Ответ сервера :')
-pprint.pprint(obj)
+
+if __name__ == "__main__":
+    id = input(' Введите id :')
+    print('Ответ сервера :')
+    pprint.pprint(user_vk(id))
